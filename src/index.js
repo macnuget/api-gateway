589fad02ebf057290a46f52b54c0c736683a4d5d
Nicola Bovolato <nicola.bovolato@gmail.com>
2021-12-16 10:12:04 +0100

    feat: used fastify-autoload

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 require('dotenv').config()
-const proxy = require('fastify-http-proxy')
+
+const path = require('path')
+
 const fastify = require("fastify")({ 
     logger: true,
 })
 
-fastify.register(proxy, {
-    upstream: process.env.WAREHOUSE_API_URL,
-    prefix: '/warehouse'
-})
-fastify.register(proxy, {
-    upstream: process.env.ORDERS_API_URL,
-    prefix: '/orders'
-})
-fastify.register(proxy, {
-    upstream: process.env.CUSTOMERS_API_URL,
-    prefix: '/customers'
-})
-fastify.register(proxy, {
-    upstream: process.env.PRODUCTS_API_URL,
-    prefix: '/products'
+fastify.register(require('fastify-autoload'), {
+    dir: path.join(__dirname, 'routes'),
+    dirNameRoutePrefix: false
 })
 
 try {
---
+++ src/routes/products.js
@@ -0,0 +1,7 @@
+module.exports = async function (fastify, opts) {
+    fastify.register(require('fastify-http-proxy'), { 
+        upstream: process.env.PRODUCTS_API_URL 
+    })
+}
+
+module.exports.autoPrefix = '/products'
