0b47b95d89983ce851b54d2dbd1f2601ebd86162
Nicola Bovolato <nicola.bovolato@gmail.com>
2021-12-16 09:58:42 +0100

    feat: Added api gateway

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('Hello world')
+require('dotenv').config()
+const proxy = require('fastify-http-proxy')
+const fastify = require("fastify")({ 
+    logger: true,
+})
+
+fastify.register(proxy, {
+    upstream: process.env.WAREHOUSE_API_URL,
+    prefix: '/warehouse'
+})
+fastify.register(proxy, {
+    upstream: process.env.ORDERS_API_URL,
+    prefix: '/orders'
+})
+fastify.register(proxy, {
+    upstream: process.env.CUSTOMERS_API_URL,
+    prefix: '/customers'
+})
+fastify.register(proxy, {
+    upstream: process.env.PRODUCTS_API_URL,
+    prefix: '/products'
+})
+
+try {
+    fastify.listen(process.env.PORT || 3000)
+} 
+catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+}
+
